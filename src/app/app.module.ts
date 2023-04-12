ff73ac770e951198ce4a2dbe1ed1a235a4413433
Fabio Oliveira <fabio@germinareagro.com.br>
2023-04-12 17:47:18 -0300

    Adicionando as telas do sistemas, login, not-found, table e dashboard (pagina inicial)

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,13 +4,22 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { NotFoundComponent } from './not-found/not-found.component';
+import { DashboardComponent } from './layout/dashboard/dashboard.component';
+import { TableComponent } from './layout/table/table.component';
+import { HeaderComponent } from './header/header.component';
+
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [	
+    AppComponent,
+    TableComponent,
+    DashboardComponent,
+    NotFoundComponent,
+      HeaderComponent
+   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
